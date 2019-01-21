bf757da53ba4e2eaf0fda325fedcdcd3197ff041
GeorgeSarkisian <32592901+Georgesarkis@users.noreply.github.com>
2019-01-21 19:09:42 +0100

    added required for input fields

--- client/scripts/custom.js
+++ client/scripts/custom.js
@@ -17,69 +17,97 @@ var app = new Vue({
     },
     methods: {
         WillAttend: function() {
-        axios
-            .post("/api/rsvp", {
-            name: this.name,
-            email: this.email,
-            numGuests: this.Number,
-            RSVPanswer: "true",
-            })
-            .then(function(response) {
-            console.log(response);
-            swal("Thank you for your response.");
-            })
-            .catch(function(error) {
-            console.log(error.response);
-            });
+            if(this.name.length == 0){
+                swal("Please write your first and last name.");
+            }else if(this.Number.length == 0){
+                swal("Please write number of people will attend.");
+            }else{
+                axios
+                .post("/api/rsvp", {
+                name: this.name,
+                email: this.email,
+                numGuests: this.Number,
+                RSVPanswer: "true",
+                })
+                .then(function(response) {
+                console.log(response);
+                swal("Thank you for your response.");
+                })
+                .catch(function(error) {
+                console.log(error.response);
+                });
+            }
         },
         WillNotAttend: function(){
-			axios
-            .post("/api/rsvp", {
-            name: this.name,
-            email: this.email,
-            numGuests: this.Number,
-            RSVPanswer: "false",
-            })
-            .then(function(response) {
-            console.log(response);
-            swal("Thank you for your response.");
-            })
-            .catch(function(error) {
-            console.log(error.response);
-            });
+            if(this.name.length == 0){
+                swal("Please write your first and last name.");
+            }else{
+                axios
+                .post("/api/rsvp", {
+                name: this.name,
+                email: this.email,
+                numGuests: this.Number,
+                RSVPanswer: "false",
+                })
+                .then(function(response) {
+                console.log(response);
+                swal("Thank you for your response.");
+                })
+                .catch(function(error) {
+                console.log(error.response);
+                });
+            }	
         },
         ConntactUs:function(){
-			axios
-            .post("/api/contact", {
-                FirstName: this.fname,
-                LastName: this.lname,
-                Email: this.email,
-                tel: this.tel,
-                Subject: this.subject,
-                Message: this.message
-            })
-            .then(function(response) {
-            console.log(response);
-            swal("We have recived your massage!Thank you.");
-            })
-            .catch(function(error) {
-            console.log(error.response);
-            });
+            if(this.fname.length == 0){
+                swal("Please write your first name.");
+            }else if(this.lname.length == 0){
+                swal("Please write your last name.");
+            }else if(this.email.length == 0 && this.tel.length == 0){
+                swal("Please write your email or telephone number.");
+            }else if(this.message.length == 0){
+                swal("Please write some message.");
+            }else{
+                axios
+                .post("/api/contact", {
+                    FirstName: this.fname,
+                    LastName: this.lname,
+                    Email: this.email,
+                    tel: this.tel,
+                    Subject: this.subject,
+                    Message: this.message
+                })
+                .then(function(response) {
+                console.log(response);
+                swal("We have recived your massage!Thank you.");
+                })
+                .catch(function(error) {
+                console.log(error.response);
+                });
+            }
         },
         SendWishes:function(){
-            axios
-            .post("/api/wishes", {
-                FirstName: this.fname,
-                LastName: this.lname,
-                Wishes: this.wishes,
-            })
-            .then(function(response) {
-            console.log(response);
-            swal("Thank you for your worm wishes");
-            })
-            .catch(function(error) {
-            console.log(error.response);
-            });
+            if(this.fname.length == 0){
+                swal("Please write your first name.");
+            }else if(this.lname.length == 0){
+                swal("Please write your last name.");
+            }else if(this.wishes.length == 0){
+                swal("Please write some wishes.");
+            }else{
+                axios
+                .post("/api/wishes", {
+                    FirstName: this.fname,
+                    LastName: this.lname,
+                    Wishes: this.wishes,
+                })
+                .then(function(response) {
+                console.log(response);
+                swal("Thank you for your worm wishes");
+                })
+                .catch(function(error) {
+                console.log(error.response);
+                });
+            }
         }
     }
 });
